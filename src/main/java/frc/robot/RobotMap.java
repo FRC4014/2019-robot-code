ab5bdbc4c244418000621f9c102cc6ad9019caaf
Nicholas <njennings2019@hightechhigh.org>
2019-01-08 13:04:52 -0800

    Switched talon numbers to ones used in 2018 robot

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -21,10 +21,11 @@ import edu.wpi.first.wpilibj.drive.MecanumDrive;
  */
 public class RobotMap {
 
-  public static WPI_TalonSRX driveTrainFrontLeftMotor = new WPI_TalonSRX(1);
-  public static WPI_TalonSRX driveTrainFrontRightMotor = new WPI_TalonSRX(2);
-  public static WPI_TalonSRX driveTrainBackLeftMotor = new WPI_TalonSRX(3);
-  public static WPI_TalonSRX driveTrainBackRightMotor = new WPI_TalonSRX(4);
+  public static WPI_TalonSRX driveTrainFrontRightMotor = new WPI_TalonSRX(1);
+  public static WPI_TalonSRX driveTrainBackRightMotor = new WPI_TalonSRX(2);
+  public static WPI_TalonSRX driveTrainFrontLeftMotor = new WPI_TalonSRX(3);
+  public static WPI_TalonSRX driveTrainBackLeftMotor = new WPI_TalonSRX(4);
+  
   public static MecanumDrive driveTrainMecanum = new MecanumDrive(driveTrainFrontLeftMotor, driveTrainBackLeftMotor, driveTrainFrontRightMotor, driveTrainBackRightMotor);
   
   //the following stuff is for a standard arcade drive for use when testing on 2018 bot
