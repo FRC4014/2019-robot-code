b4a946cb5d79d4f96aabed1a33eb7eb2bd49bacb
Jemison <njennings2019@hightechhigh.org>
2019-01-11 17:59:53 -0800

    Revert "make pixyPacket look like how javas supposed to look like"
    This reverts commit 6b28f956daf0252ea193ce5cae7079ef3abe210d.

--- src/main/java/frc/robot/vision/PixyPacket.java
+++ src/main/java/frc/robot/vision/PixyPacket.java
@@ -8,69 +8,9 @@
 package frc.robot.vision;
 
 public class PixyPacket {
-	private int Signature;
-	private int X;
-	private int Y;
-	private int Width;
-	private int Height;
-	/**
-	 * @return the signature
-	 */
-	public int getSignature() {
-		return Signature;
-	}
-	/**
-	 * @return the height
-	 */
-	public int getHeight() {
-		return Height;
-	}
-	/**
-	 * @param height the height to set
-	 */
-	public void setHeight(int height) {
-		this.Height = height;
-	}
-	/**
-	 * @return the width
-	 */
-	public int getWidth() {
-		return Width;
-	}
-	/**
-	 * @param width the width to set
-	 */
-	public void setWidth(int width) {
-		this.Width = width;
-	}
-	/**
-	 * @return the y
-	 */
-	public int getY() {
-		return Y;
-	}
-	/**
-	 * @param y the y to set
-	 */
-	public void setY(int y) {
-		this.Y = y;
-	}
-	/**
-	 * @return the x
-	 */
-	public int getX() {
-		return X;
-	}
-	/**
-	 * @param x the x to set
-	 */
-	public void setX(int x) {
-		this.X = x;
-	}
-	/**
-	 * @param signature the signature to set
-	 */
-	public void setSignature(int signature) {
-		this.Signature = signature;
-	}
+	public int Signature;
+	public int X;
+	public int Y;
+	public int Width;
+	public int Height;
 }
